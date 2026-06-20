884d6e63530f0435db5b2113a0696b004db4f4bc
Aaditya Jain <aadityajain1611@gmail.com>
2026-06-20 15:03:16 +0530

    Temporary disable migrations and seeding for Azure debugging

--- backend/InvestFlow.API/Program.cs
+++ backend/InvestFlow.API/Program.cs
@@ -1,10 +1,9 @@
 using InvestFlow.API.Extensions;
-using InvestFlow.Infrastructure.Data;
-using InvestFlow.Infrastructure.Seed;
 using InvestFlow.API.Middleware;
 using InvestFlow.Infrastructure.BackgroundJobs;
-using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(
@@ -13,7 +12,8 @@ builder.Services.AddCors(options =>
         {
             policy
                 .WithOrigins(
-                    "http://localhost:5173","http://localhost:4173")
+                    "http://localhost:5173",
+                    "http://localhost:4173")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
@@ -27,31 +27,25 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplicationServices(
     builder.Configuration);
+
 builder.Services.AddHostedService<
     MonthlyProfitBackgroundService>();
 
 builder.Services.AddHostedService<
     AnnualCompoundingBackgroundService>();
+
 var app = builder.Build();
+
 app.UseCors("Frontend");
-
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext =
-        scope.ServiceProvider
-            .GetRequiredService<ApplicationDbContext>();
-
-    await dbContext.Database.MigrateAsync();
-
-    await AdminSeeder.SeedAdminAsync(dbContext);
-}
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseMiddleware<GlobalExceptionMiddleware>();
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
@@ -59,5 +53,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/", () => "InvestFlow API Running");
 
 app.Run();
